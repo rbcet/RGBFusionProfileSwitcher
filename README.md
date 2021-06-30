fdad2bcad9eb1020de7d7dd750db4f75569c97b0
Ridvan Berkay Cetin <berkay.cetin@useinsider.com>
2021-06-30 12:48:59 +0300

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RGBFusionProfileSwitcher
+
+Use case:
+	1- Profile 1 has Color Wave.
+	2- Profile 2 has RGB off.
+	3- Running this python file will switch between these profiles.
+	4- It requires admin privileges.
---
+++ RGBFusionProfileSwitcher.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import time
+
+RGB_FUSION_PATH = 'C:/Program Files (x86)/GIGABYTE/RGBFusion/'
+
+os.system("taskkill /F /IM RGBFusion.exe")
+
+os.rename(RGB_FUSION_PATH + 'Pro1.xml', RGB_FUSION_PATH + 'Pro1.xml.old') 
+os.rename(RGB_FUSION_PATH + 'ExtPro1.xml', RGB_FUSION_PATH +  'ExtPro1.xml.old')
+
+os.rename(RGB_FUSION_PATH + 'Pro2.xml.old', RGB_FUSION_PATH + 'Pro1.xml') 
+os.rename(RGB_FUSION_PATH + 'ExtPro2.xml.old', RGB_FUSION_PATH + 'ExtPro1.xml')
+
+os.rename(RGB_FUSION_PATH + 'Pro1.xml.old', RGB_FUSION_PATH + 'Pro2.xml.old')
+os.rename(RGB_FUSION_PATH + 'ExtPro1.xml.old', RGB_FUSION_PATH + 'ExtPro2.xml.old')
+
+subprocess.Popen(["cmd.exe", "/c", RGB_FUSION_PATH + "Check_Kill.exe"], creationflags=0x08000000)
